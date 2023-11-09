e621347fcd90333f1df30d45ce9b87aed26f3e52
Alexander Willemsen <alexander.willemsen@iodigital.com>
2023-11-09 11:16:42 +0100

    fixed package.json issues

--- src/widget/ConversationalSearchPlatform.Widget/vite.config.ts
+++ src/widget/ConversationalSearchPlatform.Widget/vite.config.ts
@@ -1,6 +1,10 @@
 import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
 import * as path from "path";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +17,7 @@ export default defineConfig({
     },
     build: {
         outDir: "../dist",
+        manifest: true,
         rollupOptions: {
             input: {
                 app: 'webcomponent.html',
